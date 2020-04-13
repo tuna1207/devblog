87dbed9db72a8b29305eefc7ebe0e6dcf6f0e158
Nguyen Anh Tu <tunguyen.bhtech@gmail.com>
2020-04-13 12:19:32 +0700

    fix(setup): app - email config

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
   title: 'Dev Blog', // Required
   author: 'Personal blog by Nguyen Anh Tu', // Required
+  email: 'tunguyenanh@thetuna.tech',
   description: 'Full-stack Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://thetuna.tech',
-    email: 'tunguyenanh@thetuna.tech',
+    website: 'https://www.thetuna.tech',
     github: 'https://github.com/tunguyenbhtech',
     twitter: '',
     linkedin: 'https://www.linkedin.com/in/nguyen-anh-tu-1a171b123/',
