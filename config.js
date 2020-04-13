7260bc09f4346f17fb4eb1641405bef303638647
Nguyen Anh Tu <tunguyen.bhtech@gmail.com>
2020-04-13 10:38:45 +0700

    chore(setup): app - update link

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: 'Dev Blog', // Required
   author: 'Personal blog by Nguyen Anh Tu', // Required
-  description: 'Full-stack Web Developer',
+  description: 'Full-stack Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://thetunatech.com',
+    website: 'https://thetuna.tech',
     github: 'https://github.com/tunguyenbhtech',
     twitter: '',
     linkedin: 'https://www.linkedin.com/in/nguyen-anh-tu-1a171b123/',
